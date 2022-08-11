542f58e827ec64793f164d1b1b1dc5b9b90e71a4
Jin Choo <jinchoo315@gmail.com>
2022-08-10 23:59:43 -0500

    modified and add lessons 45, 46, 47, 48, 49, 50, 51

--- 45_changing_array_item.js
+++ 45_changing_array_item.js
@@ -1,6 +1,4 @@
-
-
-let grades = [5, 3, 8];
+let grades = [5, 3, 8]
 
 //but the first (5) needs to be changed to 6
 // 1. Ask yourself:  How can I read that item that I want to change?
@@ -9,21 +7,52 @@ let grades = [5, 3, 8];
 grades[0] // returns 5
 
 // 2. Now assing it to the new value with the equal sign (=) and the new value like so:
-grades[0] = 6;
+grades[0] = 6
 console.log(grades) // [6, 3, 8]
 
 //Examples with functions
 
 function fixGrade(grades) {
-    grades[0] = 6;
-    return grades;
+  grades[0] = 6
+  return grades
 }
 
-// The logic is similar, we just needed to add the return grades at the end of the fucntion so that we cn return the new array.  
-// Make sure to have that return grades on a new line because the code won't work. 
+// The logic is similar, we just needed to add the return grades at the end of the fucntion so that we cn return the new array.
+// Make sure to have that return grades on a new line because the code won't work.
+
+function fixGrades(grades) {
+  // This is INCORRECT
+  return (grades[0] = 6)
+}
+
+// The code above is incorrect because the function will return 6.  The reason for this is that the expression grades[0] = 6 returns 6
+// which is why the function will return 6.  However, we need it to return the new array.
+// So always perform the operation and then at the end of the function, return the array.
 
 // Increment first temperature
 function incrementFirstTemperature(temperatures) {
-    temperatures[0] = temperatures[0] + 1;
-    return temperatures;
+  temperatures[0] = temperatures[0] + 1
+  return temperatures
+}
+
+//============================================================
+
+// Change the value of the first temperature recording from -5 to -10
+
+let temperatures = [-5, 12, 15, 18, 13]
+temperatures[0] = -10
+
+// output: [-10, 12, 15, 18, 13]
+
+//============================================================
+
+//Increment first temperature
+
+// Complete the funciton incrementFirstTemperature such that:
+// 1.  It increments the first temperature from the temperatures array.  This means that if the first temperature was 5, it should become 6.  If it was 8, it should become 9.  No matter what the value was, you should increment it by 1 to the previous one.  YOu cannot hardcode any value here.
+// 2.  The function should return the new temperatures array.
+
+function incrementFirstTemperature(temperatures) {
+  temperatures[0] = temperatures[0] + 1
+  return temperatures
 }
--- 48_add_item_array.js
+++ 48_add_item_array.js
@@ -4,14 +4,14 @@
 
 // Add item to an array
 
-let numbers = [];
+let numbers = []
 
 //and you would like to add the number 10 to that array.
-numbers.push(10);
+numbers.push(10)
 
 //The .push(item) method adds(or pushes) an item into an existing array.
 
-let numbers1 = [];
+let numbers1 = []
 console.log(numbers1) // []
 
 numbers1.push(10)
@@ -24,10 +24,10 @@ numbers1.push(13)
 console.log(numbers1) // [10, 15, 13]
 
 // .length gets updated every time
-// The .length property is alwasy reflects the number of items in the array.  
+// The .length property is alwasy reflects the number of items in the array.
 
 // The .push(item) method adds (or pushes) an item into an existing array.
-// The .length property always reflects the correct number of items insdie the array.  
+// The .length property always reflects the correct number of items insdie the array.
 
 // What does .push() return?
 
@@ -35,11 +35,11 @@ console.log(numbers1) // [10, 15, 13]
 
 // .toUpperCase() method will always return a string.
 
-let name = 'alex';
+let name = 'alex'
 console.log(name.toUpperCase()) // 'ALEX'
 
-// The .push(item) method returns the new length of the array.  So it returns a number.  
-let numbers2 = [];
+// The .push(item) method returns the new length of the array.  So it returns a number.
+let numbers2 = []
 
 console.log(numbers2.push(10)) // 1 (the new length of the array)
 console.log(numbers2) // [10]
@@ -52,19 +52,19 @@ console.log(numbers2) // [10, 15, 13]
 
 // This means that if we assign the result of .push() into a variable, it will be a number representing the new length of the array.
 
-let numbers3 = [10, 20];
+let numbers3 = [10, 20]
 
 let value = numbers.push(15)
 console.log(value) // 3 (new length of the array)
 
-//we have a funciton addTemperature that receives an array of temperatures and we're expected to add the temperature 10.  
+//we have a funciton addTemperature that receives an array of temperatures and we're expected to add the temperature 10.
 //so we need to call temperatures.push(10)
-//most of the functions we write though will need to return someting or else they will return undefined.  
+//most of the functions we write though will need to return someting or else they will return undefined.
 
-// By looking at the sample usage, we can see that we expect this funciton to return the new array with the item pushed into it. 
+// By looking at the sample usage, we can see that we expect this funciton to return the new array with the item pushed into it.
 // A very common mistake
 function addTemperature(temperatures) {
-    return temperatures.push(10)
+  return temperatures.push(10)
 }
 
 //Take a moment, and think why does this NOT satifsfy the requirements of returning the new array?  What will this funcition return instead?
@@ -74,53 +74,56 @@ function addTemperature(temperatures) {
 
 //thus the correct answer is:
 function addTemperature(temperatures) {
-    temperatures.push(10)
-    return temperatures
+  temperatures.push(10)
+  return temperatures
 }
 
-// Notice how we return temperatures after we call .push().  Rather than returning the result of .push().  That's all because .push() returns the new length of the array.  
+// Notice how we return temperatures after we call .push().  Rather than returning the result of .push().  That's all because .push() returns the new length of the array.
 
+// Complete the function addGrade such that it adds the grade 18 to the grades array it receives.
 
 function addGrade(grades) {
-    grades.push(18)
-    return grades
+  grades.push(18)
+  return grades
 }
 
+// Complete the function addTemperature such that it adds the temperature -6 to the temperatures array it receives.
+
 function addTemperature1(temperatures) {
-    temperatrues.push(-6)
-    return temperatures
-};
+  temperatrues.push(-6)
+  return temperatures
+}
 
 //Pushing variables into an array
-let data = [10, 20];
-data.push(15);
+let data = [10, 20]
+data.push(15)
 console.log(data) // [10, 20, 15]
 
 // You can also push a variable into an array.
-let data1 = [10, 20];
-let number = 15;
-data1.push(number);
+let data1 = [10, 20]
+let number = 15
+data1.push(number)
 console.log(data) // [10, 20, 15]
 
-// Notice how data.push(number) works because number is a variable representing the number 15 which then goets added to the array.  
+// Notice how data.push(number) works because number is a variable representing the number 15 which then goets added to the array.
 
 // 1.submitting a grade works (it should add the grade to the grades array)
 function addGrade(grades, grade) {
-    grades.push(grade)
-    return grades
-};
+  grades.push(grade)
+  return grades
+}
 
 //2. the number of tests taken is shown.
 function getNumberofGrades(grades) {
-    return grades.length
-};
+  return grades.length
+}
 
 //3. the first submitted grade is shown (firstsubmitted, not the highest).
 function getFristGrade(grades) {
-    return grades[0]
+  return grades[0]
 }
 
 //4. the last submitted grade is shown (last submitted, not the lowest).
 function getLastGrade(grades) {
-    return grades[grades.length -1]
+  return grades[grades.length - 1]
 }
