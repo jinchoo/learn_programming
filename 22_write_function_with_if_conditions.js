26884a2a14d6f6f9ccb53fa50676ada7153de37f
Jin Choo <jinchoo315@gmail.com>
2022-08-21 14:01:31 -0500

    add lesson 22

---
+++ 22_write_function_with_if_conditions.js
@@ -0,0 +1,17 @@
+//Write functions with if conditions
+
+function getVotingMessage(age) {
+  if (age >= 18) {
+    return 'You are allowed to vote'
+  }
+}
+
+//Notice the:
+
+// 1. function definition function getVotingMessage(age)
+// 2. the opening curly brace { corresponding to the function (on the 1st line)
+// 3. the if condition if (age >= 18)
+// 4. the opening curly brace { corresponding to the if condition (on the 2nd line)
+// 5. the return 'You are allowed to vote"
+// 6. the closing curly brace } corresponding to the if condition (the closing curly barace on the 4th line closes the one opened on the 2nd line)
+// 7. the closing curly brace } corresponding to the function (the closing curly brace on the 5th line closes the one opened on the 1st line)
