76d57d8af74708a946166efdb07ed22438a05c8d
Jin Choo <jinchoo315@gmail.com>
2022-08-21 14:55:12 -0500

    add lesson 24

---
+++ 24_expressions.js
@@ -0,0 +1,19 @@
+// What is an expression?
+
+// Expressions are one of the building blokcs of the code you write.
+
+// 1 + 2
+// a + b (asuuming exisitng variables a and b)
+// year + 10 (assuming existing variable year)
+// age === 18 (assuming existing variable age)
+// age >= 18 (assuming existing variable age)
+
+// An expression is a combination of one or more variables, funcitons, and operatiors that the programming lagnuage computes to produce a value.
+
+// 1 + 2 prdouces 3
+// a + b produces the sum of a and b (assuming a dn b are numbers)
+// year + 10 produces a number which is the result of adding 10 to the variable year
+// age === 18 produces a boolean (either true or false, depending on the age)
+// age >= 18 also produces a boolean depnding on the age
+
+//===========================================================================================================================
