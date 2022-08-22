e7a78425837ef34ff9a6f884b4aea2337c520afe
Jin Choo <jinchoo315@gmail.com>
2022-08-21 23:55:05 -0500

    modified lesson 42

--- 42_reading_array_items_I.js
+++ 42_reading_array_items_I.js
@@ -6,3 +6,20 @@ grades[1] // 3
 let grades1 = [10, 5, 13]
 let firstGrade = grades1[0]
 console.log(firstGrade)
+
+// //===========================================================================================================================
+function getFullName(names) {
+  return `${names[0]} ${names[1]}`
+}
+
+// //===========================================================================================================================
+function describeShopLocation(shop, coordinates) {
+  return `The ${shop} is located at ${coordinates[0]}, ${coordinates[1]}`
+}
+
+function calculateDifference(firstRactangle, secondRectangle) {
+  return (
+    firstRectagnle[0] * firstRactangle[1] -
+    secondRectangle[0] * seoncdRectangle[1]
+  )
+}
