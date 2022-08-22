23f627343bb329744b3c1dc3385e9316c2496cc8
Jin Choo <jinchoo315@gmail.com>
2022-08-21 23:24:58 -0500

    add lesson 41

---
+++ 41_introduction_arrays.js
@@ -0,0 +1,24 @@
+// Empty Array
+
+let grades = []
+
+// This square brackets are a syntax that instructs JavaScript that this is the array data type.
+
+//===========================================================================================================================
+
+// Create Arrays
+
+let grades1 = [10, 15, 13, 10, 18]
+
+// in terms of syntax, it's straightforward, here's how it works:
+// 1. let grades = This defines a variable grades and asigns it to what's on the right of the = sign.
+// 2. [10, 15, 13, 10, 18] is an array containing 5 elements
+// 3. To create an array, you start by opening a square bracket [
+// 4. Then you list the items, separating them by a comma character,
+// 5. finally, after the last element, you need to close the ]
+
+//===========================================================================================================================
+
+// An array is a list of vlues
+// You can think of arrays as a row in an excel sheet.
+// The items inside the array are called the elements of the array.
